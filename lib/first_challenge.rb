1c9f212c029fdf5fd8b9f24ad0836422c5d82dd6
AyanaZaire <ayanazaire@gmail.com>
2018-06-18 23:30:44 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,20 @@ def first_challenge
     }
   }
 
-  #your code here
+def first_challenge
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+      knows: nil
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
 
 
   #remember to return your newly altered contacts hash!
